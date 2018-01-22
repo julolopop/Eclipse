2f359229dd07f6da86586667e18e5a08c6057a3e
julolopop <julolopop@gmail.com>
2018-01-22 14:44:59 +0100

    udp

---
+++ SocketUDP/src/com/pspro/Emisor.java
@@ -0,0 +1,56 @@
+package com.pspro;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+
+public class Emisor {
+
+	public static final int PUERTOEMISOR = 5555;
+	public static final int PUERTORECEPTOR = 4444;
+	public static final String IPEMISOR = "0.0.0.0";
+	public static final String IPRECEPTOR = "192.168.2.171";
+	//public static final String IPRECEPTOR = "192.168.3.57";
+	
+	public Emisor() {
+		DatagramSocket emisor = null;
+		DatagramPacket dgp;
+		InetAddress IPLocal;
+		InetAddress IPRemoto;
+		
+		byte[] contenido = "lolito es ...".getBytes();
+		
+		try {
+			IPLocal = InetAddress.getByName(IPEMISOR);
+			IPRemoto = InetAddress.getByName(IPRECEPTOR);
+			
+			emisor = new DatagramSocket(PUERTOEMISOR,IPLocal);//por donde sale el mensage;
+			dgp = new DatagramPacket(contenido, contenido.length,IPRemoto,PUERTORECEPTOR);
+			
+			emisor.send(dgp);
+			System.out.println("datos correctamente enviados");
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			emisor.close();
+		}
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		new Emisor();
+	}
+}
---
+++ SocketUDP/src/com/pspro/Receptor.java
@@ -0,0 +1,56 @@
+package com.pspro;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class Receptor {
+	public static final int PUERTO = 4444;
+	public static final String IPEMISOR = "0.0.0.0";
+	
+	public Receptor() {
+		DatagramSocket socket = null;
+		try {
+			socket = new DatagramSocket(PUERTO,InetAddress.getByName(IPEMISOR));
+		
+			
+			System.out.println("Emisor conectado al socker : "+socket.getLocalAddress());
+			while (true) {
+				DatagramPacket dato = new DatagramPacket(new byte[144], 144);
+				//se recibe un dato y se escribe por pantalla 
+				socket.receive(dato);//bloqueante
+				System.out.println("Recibido un paquete de : "+ dato.getAddress().getHostName() +";");
+				byte[] contenido = dato.getData();
+				
+				System.out.println(" de longitudad : "+dato.getLength());
+				
+				//deserializamos a string
+				System.out.println(new String(contenido));
+				/*
+				for (int i = 0; i < dato.getLength(); i++) {
+					System.out.print(dato.getData()[i]);
+				}
+				System.out.println();
+				*/
+				
+			}			
+		} catch (SocketException | UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			socket.close();
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		new Receptor();
+	}
+
+}
